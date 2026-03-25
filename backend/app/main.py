15560e4b46b96fdeb0d90d61f3a2b87c07bb06c7
Jatinsahu07 <jatinsahu718@gmail.com>
2026-03-25 10:11:25 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -141,13 +141,7 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "https://testslashcoder.netlify.app",
-        "https://thriving-hamster-86353b.netlify.app",
-        "https://inquisitive-paletas-5ecd40.netlify.app",
-        "https://test.slashcoder.in",
-    ],
+    allow_origins=settings.ALLOWED_ORIGINS,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
