237c5421f8477a6866e0df11a81adb3401bf070b
Jatinsahu07 <jatinsahu718@gmail.com>
2026-03-25 10:04:40 +0530

    Update config.js

--- dsa-platform-web/src/config.js
+++ dsa-platform-web/src/config.js
@@ -1,2 +1,8 @@
 // src/config.js
-export const API_BASE = process.env.REACT_APP_BACKEND_URL;
+const rawBaseUrl = import.meta.env.VITE_BACKEND_URL;
+export const API_BASE = rawBaseUrl ? rawBaseUrl.replace(/\/+$/, '') : undefined;
+
+if (!API_BASE) {
+  // eslint-disable-next-line no-console
+  console.warn('VITE_BACKEND_URL is not set. API requests will fail.');
+}
